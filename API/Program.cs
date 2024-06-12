6d7ef03f9653983308164420c0c0a00d61c6b0b8
jovantrtica <jtrtica92@gmail.com>
2024-06-12 21:38:43 +0200

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,21 @@
+using API.Extentions;
+using Application.Activities;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddControllers(); // Add controller services
 
 builder.Services.AddAuthorization(); // Add authorization services
-builder.Services.AddControllers(); // Add controller services
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+
+
 
 var app = builder.Build();
 
---
+++ Application/Activities/Create.cs
@@ -0,0 +1,42 @@
+using System.Threading;
+using MediatR;
+using Domain;
+using System.Threading.Tasks;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+            {
+
+                public Activity Activity { get; set; }
+
+            }
+
+            public class Handler : IRequestHandler<Command>
+
+            {
+                
+                private readonly DataContext _context;
+                public Handler(DataContext context)
+
+                {
+                        _context = context;
+                }
+                public async Task Handle(Command request, CancellationToken cancellationToken)
+
+                    {
+                        _context.Activities.Add(request.Activity);
+
+                        await _context.SaveChangesAsync();
+
+            
+                    }
+
+
+            }
+
+    }
+}
